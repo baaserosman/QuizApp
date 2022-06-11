8d9da9dcf23ce11cdfc864e8c45ee0c3ee15047a
baaserosman <baaser.osman@gmail.com>
2022-06-11 10:16:48 +0200

    views

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import CreateAPIView
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 class RegisterAPIs(CreateAPIView):
     queryset = User.objects.all()
@@ -10,10 +11,28 @@ class RegisterAPIs(CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = RegisterSerializer(data=request.data)
-        serializer.is_valid(raise_exceptions=True)
-        serializer.save()
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.create(user=user)
+        print(token.key)
         return Response(
             {
-                "messega" : "User created successfully."
-            }
+                "messega" : "User created successfully.",
+                "token" : token.key,
+              }
         )
+        # if serializer.is_valid() :
+        #     user = serializer.save()
+        #     token = Token.objects.create(user=user)
+        #     print(token.key)
+        #     return Response(
+        #         {
+        #             "messega" : "User created successfully.",
+        #             "token" : token.key,
+        #         }
+        #     )
+        # return Response(
+        #         {
+        #             "messega" : "Invalid Data. Try again!",
+        #         }
+        # )
