af310666bbed33585773f69c750445b4e2910696
baaserosman <baaser.osman@gmail.com>
2022-06-23 00:17:54 +0200

    ,

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Answer, Category, Question, Quiz
 import nested_admin
 
-#answer başka tablonun içinde olacak şekile inlien olarak hazırla
+# answer başka tablonun içinde olacak şekile inlien olarak hazırla
 class Answerinline(nested_admin.NestedTabularInline):
   model = Answer
   extra = 4
-  max_num = 4
+  max_num =4
 class Questioninline(nested_admin.NestedTabularInline):
   model = Question
   inlines = [Answerinline]
--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from .models import Question, Quiz, Category, Answer
 
-
-
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
